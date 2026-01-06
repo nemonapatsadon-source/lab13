07cdca9fd5fd06fe0f03a5bd51458cecc60b6d0e
nemonapatsadon-source <nemonapatsadon@gmail.com>
2026-01-06 22:58:23 +0700

    lab13-2

--- lab13_2.cpp
+++ lab13_2.cpp
@@ -21,3 +21,31 @@ int main()
     }while(s != 0 || x != 0 || y != 0);
     return 0;
 }
+
+void showImage(const bool B[][M]){
+    cout << "------------------------------------------------------------------------\n";
+    for(int j = 0 ; j < N ; j++){
+        cout << "|";
+        for(int i = 0 ; i < M ; i++){
+            if(B[j][i] == 0){
+                cout << " ";
+            } else{
+                cout << "*";
+            }
+        }
+        cout << "|\n";
+    }
+    cout << "------------------------------------------------------------------------\n";
+
+}
+
+void updateImage(bool B[][M],int s,int x, int y){
+    for(int j = 0; j < N ; j++){
+        for(int i = 0 ; i < M ;i++){
+            if(sqrt(pow(j-x, 2.0) + pow(i-y, 2.0)) <= s - 1){
+                B[j][i] = 1 ;
+            }
+        }
+    }
+}
+
