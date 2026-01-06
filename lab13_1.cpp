19c4114d4a7d49b90eda0dd97e7ce498e8ace686
nemonapatsadon-source <nemonapatsadon@gmail.com>
2026-01-06 21:14:18 +0700

    lab13-1

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -19,3 +19,60 @@ int main(){
     cout << "Min = " << B[5];
     return 0;
 }
+
+void stat(const double Num[],int Size,double Ans[]){
+    int i = 0;
+    double Asum = 0;
+    
+    while (i < Size)
+    {
+        Asum += Num[i];
+        i++;
+    }
+    
+    double Amean = Asum/Size;
+
+    double Ssum = 0;
+    i = 0;
+
+    while(i < Size){
+        Ssum += pow((Num[i]-Amean),2);
+        i++;
+    }
+
+    double Sd = pow((Ssum/Size),0.5);
+
+    double Gsum = 1;
+    i = 0;
+
+    while (i < Size)
+    {
+        Gsum *= Num[i];
+        i++;
+    }
+    
+    double Gmean = pow(Gsum, 1.0 / Size);
+
+    double Hsum = 0;
+    i=0;
+
+    while (i < Size)
+    {
+        Hsum += (1/Num[i]);
+        i++;
+    }
+    
+    double Hmean = Size/Hsum;
+
+    double Max = Num[0],Min = Num[0];
+    for(i=0;i<Size;i++){
+        if(Num[i]>Max){ Max = Num[i];}
+        if(Num[i]<Min){ Min = Num[i];}
+    }
+    Ans[0] = Amean;
+    Ans[1] = Sd;
+    Ans[2] = Gmean;
+    Ans[3] = Hmean;
+    Ans[4] = Max;
+    Ans[5] = Min;
+}
